47b5b0236bb45cf7c208b5beb1ad963a945a4f8c
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 20:11:05 +0800

    09-functions-in-functions

--- app.js
+++ app.js
@@ -84,12 +84,25 @@ startGameBtn.addEventListener('click', () => {
 // not related to game
 
 const sumUp = (a, b, ...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
   let sum = 0;
   for (const num of numbers) {
-    sum += num;
+    sum += validateNumber(num);
   }
   return sum;
 };
 
-console.log(sumUp(1, 5, 10, -3, 6, 10));
+const subtractUp = function() {
+  let sum = 0;
+  for (const num of arguments) { // don't use that
+    sum -= num;
+  }
+  return sum;
+}
+
+console.log(sumUp(1, 5, 'fdsa', -3, 6, 10));
 console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
