dcd765753eb39f8f327889070343c2d7d7c9a17c
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 20:49:12 +0800

    09-triggering-events-programmatically

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -46,8 +46,25 @@ div.addEventListener('mouseenter', event => {
   console.log(event);
 });
 
-button.addEventListener('mouseenter', event => {
+button.addEventListener('click', event => {
   event.stopPropagation();
   console.log('CLICKED BUTTON');
   console.log(event);
+});
+
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach(listItem => {
+//   listItem.addEventListener('click', event => {
+//     event.target.classList.toggle('highlight');
+//   });
+// });
+
+list.addEventListener('click', event => {
+  // console.log(event.currentTarget);
+  // event.target.classList.toggle('highlight');
+  event.target.closest('li').classList.toggle('highlight');
+  // form.submit();
+  button.click();
 });
