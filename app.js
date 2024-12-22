92b7a1894cdc548d33d524a8545292ea9c82fd23
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 20:53:03 +0800

    03-factory-functions

--- app.js
+++ app.js
@@ -31,3 +31,17 @@ function printHobbies(h) {
 }
 
 printHobbies(hobbies);
+
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+
+  return calculateTax;
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateVatAmount(200));
