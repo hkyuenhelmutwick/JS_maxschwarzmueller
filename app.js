5e706101174b68f30ba6c86e2c4818ce5840b20b
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 23:49:05 +0800

    05-async-starting-setup

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-const { checkAndGenerate, createElement } = require('./util');
+const { fetchData } = require('./http');
 
-const initApp = () => {
-  // Initializes the app, registers the button click listener
-  const newUserButton = document.querySelector('#btnAddUser');
-  newUserButton.addEventListener('click', addUser);
+const button = document.querySelector('button');
+
+const loadTitle = () => {
+  return fetchData().then(extractedData => {
+    const title = extractedData.title;
+    const transformedTitle = title.toUpperCase();
+    return transformedTitle;
+  });
 };
 
-const addUser = () => {
-  // Fetches the user input, creates a new HTML element based on it
-  // and appends the element to the DOM
-  const newUserNameInput = document.querySelector('input#name');
-  const newUserAgeInput = document.querySelector('input#age');
-
-  const outputText = checkAndGenerate(
-    newUserNameInput.value,
-    newUserAgeInput.value
-  );
-
-  if (!outputText) {
-    return;
-  }
-
-  const userList = document.querySelector('.user-list');
-
-  const element = createElement('li', outputText, 'user-item');
-  userList.appendChild(element);
+const printTitle = () => {
+  loadTitle().then(title => {
+    console.log(title);
+  });
 };
 
-// Start the app!
-initApp();
+button.addEventListener('click', printTitle);
+
+exports.printTitle = printTitle;
