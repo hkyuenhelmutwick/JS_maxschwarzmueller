b82c97a9ee58411037d947388f646eea28138f46
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 20:53:19 +0800

    04-closures

--- app.js
+++ app.js
@@ -32,9 +32,12 @@ function printHobbies(h) {
 
 printHobbies(hobbies);
 
+let multiplier = 1.1;
+
 function createTaxCalculator(tax) {
   function calculateTax(amount) {
-    return amount * tax;
+    console.log(multiplier);
+    return amount * tax * multiplier;
   }
 
   return calculateTax;
@@ -42,6 +45,8 @@ function createTaxCalculator(tax) {
 
 const calculateVatAmount = createTaxCalculator(0.19);
 const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+// multiplier = 1.2;
 
 console.log(calculateVatAmount(100));
 console.log(calculateVatAmount(200));
