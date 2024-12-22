34dbc7d12583b5cf07ceed3e2d35544c78447398
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 20:06:40 +0800

    03-strict-mode

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-let name = 'Max';
+// let name = 'Max';
 
-if (name === 'Max') {
-  let hobbies = ['Sports', 'Cooking'];
-  console.log(hobbies);
+// if (name === 'Max') {
+//   let hobbies = ['Sports', 'Cooking'];
+//   console.log(hobbies);
 
-}
+// }
 
-function greet() {
-  let age = 30;
-  let name = 'Manuel';
-  console.log(name, age, hobbies);
-}
+// function greet() {
+//   let age = 30;
+//   let name = 'Manuel';
+//   console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies);
+// console.log(name, hobbies);
 
-greet();
+// greet();
+'use strict';
+
+const userName = 'Max';
+var undefined = 5;
+
+console.log(userName);
+
