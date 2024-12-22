375a368ff47a3c22d4f12d40bc450ce5a81411a9
HKYuen Helmut Wick <hkyuenhelmutwick@protonmail.com>
2024-12-22 20:34:17 +0800

    07-base-component-class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,6 +12,33 @@ class Product {
   }
 }
 
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId) {
+    this.hookId = renderHookId;
+  }
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return this.rootElement;
+  }
+}
+
 class ShoppingCart {
   items = [];
 
